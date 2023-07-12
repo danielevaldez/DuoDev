0f961bb0784e80ba40f3bf1f15bdd03366dfe295
Daniel Valdez <devaldez4@gmail.com>
2023-07-11 20:53:20 -0500

    File IO

--- BMICalculator/src/main/java/org/example/Recommendation.java
+++ BMICalculator/src/main/java/org/example/Recommendation.java
@@ -5,3 +5,4 @@ public class Recommendation {
 /*this is where I'd like to my a FILE that outputs user recommendations based on their BMI
 * I also want to add recommended weight ranges for user height and also a calculator for recommended calorie intake if the user is trying to lose or gain weight*/
 }
+//**FILE i/o here
